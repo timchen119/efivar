ac05e53cec913cb4abd75e7583c4eb59dade2c15
Jian-Ding Chen (timchen119) <tim.chen119@canonical.com>
2018-06-28 16:10:19 +0800

    need make_hd_dn_udev

--- src/creator.c
+++ src/creator.c
@@ -306,6 +306,17 @@ efi_va_generate_file_device_path_from_esp(uint8_t *buf, ssize_t size,
 		}
 		off += sz;
 	}
+	
+	if (fd < 0 && !dev && (options & EFIBOOT_ABBREV_HD)) {
+		syslog(LOG_CRIT, "test4 + dependency udev");
+		
+		sz = make_hd_dn_udev(buf+off, size?size-off:0, off, devpath, options);
+		if (sz < 0) {
+			syslog(LOG_CRIT,"could not make HD() DP node from udev");
+			goto err;
+		}		
+		off += sz;
+	}
 
 	char *filepath = strdupa(relpath);
 	tilt_slashes(filepath);
